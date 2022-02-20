153206b65bfd24dd65fff25455effdce3ac07201
EverSeenTOTOTO <everseenflash@163.com>
2022-02-20 17:30:02 +0800

    fix stop threads

--- src/actor.rs
+++ src/actor.rs
@@ -71,34 +71,40 @@ pub struct ActorCounter {
     dispatcher: String,
     reducer: String,
     mail: SimpleChannelMail,
+    main_tx: crossbeam_channel::Sender<String>,
 }
 
+
 impl ActorCounter {
-    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, dispatcher: &str, reducer: &str) -> ActorCounter {
+    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, dispatcher: &str, reducer: &str, main_tx: crossbeam_channel::Sender<String>) -> ActorCounter {
         ActorCounter {
             address: address.to_string(),
             mail: SimpleChannelMail::new(address, rx, tx),
             dispatcher: dispatcher.to_string(),
             reducer: reducer.to_string(),
+            main_tx
         }
     }
 
+    // 计数器，收到count指令后计算质数，每算出一个向reducer发送一个
     pub fn run(&mut self) {
         println!("start {}, dispatcher is: {}, reducer is: {}", self.address, self.dispatcher, self.reducer);
         self.ready();
         loop {
-            std::thread::sleep(std::time::Duration::from_millis(300));
+            // std::thread::sleep(std::time::Duration::from_millis(300));
             if let Some(packet) = self.mail.recv() {
                 match packet.command.as_str() {
-                    "count" => {
+                    "count range" => {
                         let range = packet.data.split(",").collect::<Vec<&str>>();
                         self.count(range[0].parse::<u64>().unwrap(), range[1].parse::<u64>().unwrap());
                     },
                     "done" => {
-                        println!("{} stopped", self.address);
+                        self.main_tx.send(self.address.clone()).unwrap();
                         break;
                     },
-                    _ => {}
+                    _ => {
+                        println!("{} unknown command: {}", self.address, & packet.command);
+                    }
                 }
             }
         };
@@ -109,16 +115,17 @@ impl ActorCounter {
 
         for i in from..to {
             if is_prime(i) {
-                self.mail.send("reducer", "value", &format!("{}", i));
+                self.mail.send("reducer", "prime value", &format!("{}", i));
             }
         }
 
+        // 单次任务已完成
         self.ready();
     }
 
     fn ready(&self) {
         println!("{} ready", self.address);
-        self.mail.send(&self.dispatcher, "worker", &self.address);
+        self.mail.send(&self.dispatcher, "worker ready", &self.address);
     }
 }
 
@@ -129,36 +136,39 @@ pub struct ActorReducer {
     pub received_primes: Vec<String>,
     dispatcher: String,
     mail: SimpleChannelMail,
+    main_tx: crossbeam_channel::Sender<String>,
 }
 
 impl ActorReducer {
-    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, required_prime_count: u64, dispatcher: &str) -> ActorReducer {
+    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, required_prime_count: u64, dispatcher: &str, main_tx: crossbeam_channel::Sender<String>) -> ActorReducer {
         ActorReducer {
             address: address.to_string(),
             required_prime_count,
             received_primes: Vec::new(),
             mail: SimpleChannelMail::new(address, rx, tx),
             dispatcher: dispatcher.to_string(),
+            main_tx
         }
     }
 
+    // 收集器，收集质数到一定数目之后提交给dispatcher
     pub fn run(&mut self) {
         println!("start {}", self.address);
         loop {
             // std::thread::sleep(std::time::Duration::from_millis(300));
             if let Some(packet) = self.mail.recv() {
                 match packet.command.as_str() {
-                    "value" => {
+                    "prime value" => {
                         println!("{} got {}", self.address, packet.data);
                         self.received_primes.push(packet.data);
                     },
-                    _ => {}
+                    _ => {
+                        println!("{} unknown command: {}", self.address, & packet.command);
+                    }
                 }
             }
 
             if self.received_primes.len() >= self.required_prime_count as usize {
-                println!("{} done", self.address);
-
                 // TODO: sort
                 let result = self.received_primes
                     .clone()
@@ -166,6 +176,7 @@ impl ActorReducer {
                     .reduce(|p, c| {
                         return format!("{},{}", p, c);
                     });
+ 
                 // 发送结束信号
                 match result {
                     Some(r) => {
@@ -175,6 +186,8 @@ impl ActorReducer {
                         self.mail.send(&self.dispatcher, "done", "none");
                     }
                 }
+
+                self.main_tx.send(self.address.clone()).unwrap();
                 break;
             }
         }
@@ -186,42 +199,58 @@ pub struct ActorDispatcher {
     pub address: String,
     pub range_start: u64,
     pub offset: u64,
+    main_tx: crossbeam_channel::Sender<String>,
+    all_workers: Vec<String>, // 仅用于结束时停止worker
     mail: SimpleChannelMail,
 }
 
 impl ActorDispatcher {
-    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, range_start: u64, offset: u64) -> ActorDispatcher {
+    pub fn new(address: &str, rx: crossbeam_channel::Receiver<String>, tx: crossbeam_channel::Sender<String>, range_start: u64, offset: u64, main_tx: crossbeam_channel::Sender<String>) -> ActorDispatcher {
         ActorDispatcher {
             range_start,
             offset,
             address: address.to_string(),
             mail: SimpleChannelMail::new(address, rx, tx),
+            all_workers: Vec::new(),
+            main_tx
         }
     }
 
+    pub fn save_worker_address(&mut self, address: &str) {
+        self.all_workers.push(address.to_string());
+    }
+
+    // 分配器，在收到worker就绪的请求时分派任务，收到reducer完成的请求时终止
     pub fn run(&mut self) {
         println!("start {}", self.address);
         loop {
-            std::thread::sleep(std::time::Duration::from_millis(300));
+            // std::thread::sleep(std::time::Duration::from_millis(300));
             if let Some(packet) = self.mail.recv() {
                 match packet.command.as_str() {
-                    "worker" => {
+                    "worker ready" => {
                         println!("{} got {}", self.address, packet.data);
                         self.notify(&packet.data);
                     },
                     "done" => {
                         println!("final result: {}", packet.data);
-                        println!("{} stopped", self.address);
+
+                        for w in self.all_workers.clone() {
+                            self.mail.send(&w, "done", "");
+                        }
+
+                        self.main_tx.send(self.address.clone()).unwrap();
                         break;
                     },
-                    _ => {}
+                    _ => {
+                        println!("{} unknown command: {}", self.address, & packet.command);
+                    }
                 }
             }
         }
     }
 
     fn notify(&mut self, worker: &str) {
-        self.mail.send(&worker, "count", &format!("{},{}", self.range_start, self.range_start + self.offset));
+        self.mail.send(&worker, "count range", &format!("{},{}", self.range_start, self.range_start + self.offset));
         self.range_start += self.offset;
     }
 }
